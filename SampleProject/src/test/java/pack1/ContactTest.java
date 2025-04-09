628d7b32e341c2bcc86a6d6017b102a4789c6bcc
manas <manas@Manasa>
2025-04-09 11:45:25 +0530

    first push

---
+++ SampleProject/src/test/java/pack1/ContactTest.java
@@ -0,0 +1,29 @@
+package pack1;
+
+import org.testng.annotations.Test;
+
+public class ContactTest {
+
+	@Test
+	public void createcontacttest() {
+
+		String URL = System.getProperty("url");
+		String BROWSER = System.getProperty("browser");
+		String USERNAME = System.getProperty("username");
+		String PASSWORD = System.getProperty("password");
+
+		System.out.println(URL);
+		System.out.println(BROWSER);
+		System.out.println(USERNAME);
+		System.out.println(PASSWORD);
+		System.out.println("execute createcontacttest ");
+
+	}
+
+	@Test
+	public void modifycontacttest() {
+		System.out.println("execute modifycontacttest ");
+
+	}
+
+}
